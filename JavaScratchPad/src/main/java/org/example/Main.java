c390542c7326c116717d978b5b92451756829d72
BJ <brahmjyotsingh@gmail.com>
2023-03-16 16:56:57 +0530

    Implemented Linked List

--- JavaScratchPad/src/main/java/org/example/Main.java
+++ JavaScratchPad/src/main/java/org/example/Main.java
@@ -1,8 +1,25 @@
 package org.example;
 
+import org.LinkList.LinkList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Is Palindrome:"+isPalindrome(121));
+
+        LinkList llist = new LinkList();
+        llist.print();
+
+        for(int i=6;i>=1;i--){
+            llist.insert(i);    //
+        }
+
+        llist.print();
+        llist.reverse();
+        System.out.println("After Reversing");
+        llist.print();
+        System.out.println("Inserting 1000 at index 4");
+        llist.insert(1000,4);
+        llist.print();
     }
 
     public static boolean isPalindrome(int num){
