2149ef481f3f500d6155ffc4d7ef2fc312a7506d
BJ82 <brahmjyotsingh@gmail.com>
2024-02-01 05:07:57 +0530

    Refactoring

--- JavaScratchPad/src/main/java/org/designpattern/visitor/TotalFlatCalc.java
+++ JavaScratchPad/src/main/java/org/designpattern/visitor/TotalFlatCalc.java
@@ -1,6 +1,7 @@
 package org.designpattern.visitor;
 
 import org.designpattern.Composite.Building;
+import org.designpattern.Composite.Flat;
 import org.designpattern.Composite.Society;
 
 import java.lang.reflect.Field;
@@ -13,17 +14,17 @@ public class TotalFlatCalc implements Visitor{
 
         int totalFlats=0;
         try{
-            Class soctyObj = Class.forName("org.designpattern.Composite.Society");
+            Class<?> soctyObj = Class.forName("org.designpattern.Composite.Society");
             Field buildings = soctyObj.getDeclaredField("buildings");
             buildings.setAccessible(true);
-            java.util.List listOfBldgn = (java.util.List) buildings.get(s);
-            for(Object b:listOfBldgn){
-                totalFlats += (int)visit((Building) b);
+            java.util.List<Building> listOfBldgn = (java.util.List) buildings.get(s);
+            for(Building b:listOfBldgn){
+                totalFlats += (int)visit(b);
             }
 
         }
         catch(ClassNotFoundException | NoSuchFieldException | IllegalAccessException e){
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
         return totalFlats;
     }
@@ -31,19 +32,21 @@ public class TotalFlatCalc implements Visitor{
     @Override
     public Object visit(Building b) {
 
-        java.util.List listOfFlats = null;
+        java.util.List<Flat> listOfFlats = null;
         try{
-            Class bldngObj = null;
+            Class<?> bldngObj = null;
             bldngObj = Class.forName("org.designpattern.Composite.Building");
             Field flats = null;
             flats = bldngObj.getDeclaredField("flats");
             flats.setAccessible(true);
             listOfFlats = (java.util.List) flats.get(b);
+            return listOfFlats.size();
 
         }
         catch(ClassNotFoundException | NoSuchFieldException | IllegalAccessException e){
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
-        return listOfFlats.size();
+
+        return null;
     }
 }
